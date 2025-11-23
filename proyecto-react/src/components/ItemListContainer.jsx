92378a9bf8495eac0c32c5b9f531396a52e25c5f
HuisiCristian <162733430+HuisiCristian@users.noreply.github.com>
2025-11-23 01:22:47 -0300

    modificaciones

--- proyecto-react/src/components/ItemListContainer.jsx
+++
@@ -1,8 +0,0 @@
-import './ItemListContainer.scss';
-export default function ItemListContainer() {
-    return (
-        <div>
-            <h2>Bienvenidos a Gato Negro</h2>
-        </div>
-    )
-}
