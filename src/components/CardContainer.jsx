49702d360b0cf7eb2fa7f4835051a91ffe29139c
HuisiCristian <162733430+HuisiCristian@users.noreply.github.com>
2026-02-21 13:36:40 -0300

    commit

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,19 +1,39 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import cartContext from "../context/cartContext"
 import { createBuyOrder } from "../data/firestore"
 
 export default function CardContainer(props) {
     const {cart,removeItemFromCart} = useContext(cartContext)
+    const [formData, setFormData] = useState({username:"" ,email: "", telefono: ""  })
+
+    console.log(formData)
 
     function handleCheckout() {
         const buyOrder = {
-            buyer : "Gato Negro",
+            buyer : formData,
             items : cart,
             total : cart.reduce((total, item) => total + item.price * item.count, 0),
             date : new Date().toLocaleString()
         }
 
         createBuyOrder(buyOrder)
+    }
+
+    function handleSumit(event){
+        event.preventDefault()
+
+    }
+
+    function handleChange(event){
+        const {value, name} = event.target
+        const newFormData = {...formData}
+        newFormData[name] = value
+        setFormData (newFormData)
+
+    }
+
+    function handleReset(){
+        setFormData({username:"" ,email: "", telefono: ""  })
     }
 
 
@@ -36,14 +56,58 @@ export default function CardContainer(props) {
                     </ul>
                 </div>
 
-                {cart.length > 0 ? (
-                    <button onClick={handleCheckout}>Confirmar compra</button>
-                ) : (
-                    <p>No hay productos para comprar.</p>
-                )}
-        </div>
+                
 
-        </div>
+                <div className="checkout-form" >    
+                    <form onSubmit={handleSumit}>
+
+                        <label htmlFor="username">
+                            Nombre de usuario:
+                            <input onChange={handleChange} 
+                            name="username" 
+                            type="text" 
+                            placeholder="Nombre Completo" 
+                            value = {formData.username} 
+                            />
+                        </label>
+
+                        <label htmlFor="email">
+                            Correo electronico:
+                            <input onChange={handleChange} 
+                            name="email" 
+                            type="email" 
+                            placeholder="example@gmail.com"
+                            value = {formData.email}
+                            />
+                            
+                        </label>
+
+                        <label htmlFor="telefono">
+                            Telefono:
+                            <input onChange={handleChange} 
+                            name="telefono" 
+                            type="text" 
+                            placeholder="123456" 
+                            value = {formData.telefono}
+                            />
+                        </label>
+
+                        {cart.length > 0 ? (
+                            <button onClick={handleCheckout}>Confirmar compra</button>
+                        ) : (
+                            <p>No hay productos para comprar.</p>
+                        )}
+
+                        <button onClick={handleReset} type="reset"></button>
+
+                    </form>
+
+                </div>
+                
+            </div>
+
+
+        </div>  
     )
 }
 
